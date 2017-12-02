10c19f1ee4a029dbb4cddf7f61e48b32b4e61405
StephanGELo <lefortsge@gmail.com>
2017-12-01 20:45:27 -0800

    Attempt LinkedLists.js; currently unfinished

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,51 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //takes a value and adds it to the end of the list
+    // this.head = new Node(value);
+    var newTail = new Node(value);
+
+    if (this.head === null) {
+      this.head = newTail; 
+    }
+    if (this.tail !== null) {
+      this.tail.next = newTail;
+    }
+    
+    this.tail = newTail;
   };
 
-  list.removeHead = function() {
+  list.removeHead = function() { 
+    // //removes the first node from the list and returns its value
+    if (this.head === null) {
+      return;
+    }  
+
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    }
+    console.log(list);
+    // this.head = this.head.value;
+    var result = this.head;
+    this.head = this.head.next;
+    // this.head.next = this.head.value;
+    //this.head. = this.head;
+    // return this.head;
+    // var currentValue = this.head.value;
+    // this.head.next = this.head.value; s
+
+    // // this.head = null;
+    
+    // // return currentValue;
+    // this.head = null;
+    // this.tail = null;
+    // return result;
   };
 
   list.contains = function(target) {
+    //returns boolean reflecting whether or not the passed in value 
+    // is in the list.
   };
 
   return list;
