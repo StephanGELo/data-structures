0cc404d03d68d410827f149607cc407471cffe36
StephanGELo <lefortsge@gmail.com>
2017-12-02 14:24:47 -0800

    Pass five tests for tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,12 +11,24 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var tree = Tree(value);  
+  this.children.push(tree);
 };
 
 treeMethods.contains = function(target) {
+  if (target === this.value) {
+    return true;
+  }
+
+  if (this.children.length > 0) {
+    for (var i = 0; i < this.children.length; i++) {
+      if (target === this.children[i].value) {
+        return true;
+      }
+      return false;     
+    }
+  }
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
